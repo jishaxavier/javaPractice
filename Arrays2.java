8a2b28d294708fb91c9a2cf17c8906b1889de5cc
Jisha Xavier <xavierjisha@gmail.com>
2021-04-05 11:02:51 -0400

    Add files via upload

---
+++ Arrays2.java
@@ -0,0 +1,17 @@
+package sampleException;
+
+public class Arrays2 {
+
+	public static void main(String[] args) {
+		String str = "Collecting Words";
+		String [] str1=str.split(" ");
+		System.out.println(str1[1]);
+		
+		
+		
+		
+		
+
+	}
+
+}
